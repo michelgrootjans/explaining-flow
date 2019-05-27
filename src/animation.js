2d0474a3441b3221f035a7da65693ec152e3adc4
Michel Grootjans <michel.grootjans@persgroep.net>
2019-05-27 09:45:08 +0200

    refactor

--- src/animation.js
+++ src/animation.js
@@ -4,11 +4,11 @@ const $ = require('jquery');
 (function () {
   const initialize = () => {
     PubSub.subscribe('worklist.created', (topic, subject) => {
-      const $column = $('<li/>');
-      $column.addClass('column');
-      $column.attr('data-column-id', subject.id);
-      $column.append($('<h2/>').text(subject.name));
-      $column.append($('<ul/>').addClass('cards'));
+      const $column = $('<li/>')
+        .addClass('column')
+        .attr('data-column-id', subject.id)
+        .append($('<h2/>').text(subject.name))
+        .append($('<ul/>').addClass('cards'));
 
       console.log({topic, subject})
 
