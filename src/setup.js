3456a2879e23ae85dc4f71fe2d3c87bc606a1950
Michel Grootjans <michel.grootjans@persgroep.net>
2019-05-28 10:43:13 +0200

    refactor

--- src/setup.js
+++ src/setup.js
@@ -1,17 +1,24 @@
 require('./animation').initialize();
 const {Worker, WorkItem, WorkList} = require('./worker');
 
-let inbox = new WorkList('inbox');
-let inProgress = new WorkList('in progress');
-let outbox = new WorkList('outbox');
+let backlog = new WorkList('backlog');
+let dev = new WorkList('development');
+let readyForReview = new WorkList('ready for review');
+let review = new WorkList('review');
+let readyForQA = new WorkList('ready for QA');
+let qa = new WorkList('QA');
+let prod = new WorkList('done');
 
-inbox.add(new WorkItem(1000));
-inbox.add(new WorkItem(1000));
-inbox.add(new WorkItem(1000));
-// inbox.add(new WorkItem(1000));
-// inbox.add(new WorkItem(1000));
-// inbox.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
+backlog.add(new WorkItem(1000));
 
 setTimeout(() => {
-  new Worker(inbox, inProgress, outbox).work();
+  new Worker(backlog, dev, readyForReview).work();
+  new Worker(readyForReview, review, readyForQA).work();
+  new Worker(readyForQA, qa, prod).work();
 }, 1000);
