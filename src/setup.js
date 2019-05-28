2f45726e46ba542347d72bd7a441998178c457cd
Michel Grootjans <michel.grootjans@persgroep.net>
2019-05-28 15:08:51 +0200

    refactor

--- src/setup.js
+++ src/setup.js
@@ -1,24 +1,24 @@
-// require('./animation').initialize();
-// const {Worker, WorkItem, WorkList} = require('./worker');
-//
-// let backlog = new WorkList('backlog');
-// let dev = new WorkList('development');
-// let readyForReview = new WorkList('-');
-// let review = new WorkList('review');
-// let readyForQA = new WorkList('-');
-// let qa = new WorkList('QA');
-// let prod = new WorkList('done');
-//
-// function generateTaskDuration(duration) {
-//   return Math.floor((Math.random() * duration) + 1);;
-// }
-//
-// for (let i = 0; i < 100; i++) {
-//   backlog.add(new WorkItem(generateTaskDuration(2000)));
-// }
-//
-// setTimeout(() => {
-//   new Worker(backlog, dev, readyForReview, 1).work();
-//   new Worker(readyForReview, review, readyForQA, 0.9).work();
-//   new Worker(readyForQA, qa, prod, 0.8).work();
-// }, 1000);
+require('./animation').initialize();
+const {Worker, WorkItem, WorkList} = require('./worker');
+
+let backlog = new WorkList('backlog');
+let dev = new WorkList('development');
+let readyForReview = new WorkList('-');
+let review = new WorkList('review');
+let readyForQA = new WorkList('-');
+let qa = new WorkList('QA');
+let prod = new WorkList('done');
+
+function generateTaskDuration(duration) {
+  return Math.floor((Math.random() * duration) + 1);;
+}
+
+for (let i = 0; i < 100; i++) {
+  backlog.add(new WorkItem(generateTaskDuration(2000)));
+}
+
+setTimeout(() => {
+  new Worker(backlog, dev, readyForReview, 1).work();
+  new Worker(readyForReview, review, readyForQA, 0.9).work();
+  new Worker(readyForQA, qa, prod, 0.8).work();
+}, 1000);
