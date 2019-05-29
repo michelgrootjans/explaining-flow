ca0fd1b918cd7234afbbdd41bc8cd92d2146ba55
Michel Grootjans <michel.grootjans@persgroep.net>
2019-05-29 10:53:22 +0200

    refactor

--- spec/worker.spec.js
+++ spec/worker.spec.js
@@ -20,65 +20,63 @@ describe('a worker', () => {
   });
 
   describe('with one task', () => {
-    it('works on the item', () => {
-      let workItem = new WorkItem(1000);
+    beforeEach(() => {
+      workItem = new WorkItem(1000);
       worker.work();
       inbox.add(workItem);
+    });
+    it('works on the item', () => {
+      jest.advanceTimersByTime(999);
 
-      jest.advanceTimersByTime(500);
       expect(inbox.items()).toEqual([]);
       expect(inProgress.items()).toEqual([workItem]);
       expect(outbox.items()).toEqual([]);
     });
 
     it('finished the item', () => {
-      let workItem = new WorkItem(1000);
-      worker.work();
-      inbox.add(workItem);
-
       jest.advanceTimersByTime(1000);
+
       expect(inbox.items()).toEqual([]);
       expect(inProgress.items()).toEqual([]);
       expect(outbox.items()).toEqual([workItem]);
     });
-
   });
 
   describe('with two tasks', () => {
-    it('works on the first item', () => {
-      let workItem1 = new WorkItem(1000);
-      let workItem2 = new WorkItem(500);
+    beforeEach(() => {
+      workItem1 = new WorkItem(1000);
+      workItem2 = new WorkItem(500);
       worker.work();
       inbox.add(workItem1);
       inbox.add(workItem2);
+    });
+    it('works on the first item', () => {
+      jest.advanceTimersByTime(999);
 
-      jest.advanceTimersByTime(500);
       expect(inbox.items()).toEqual([workItem2]);
       expect(inProgress.items()).toEqual([workItem1]);
       expect(outbox.items()).toEqual([]);
     });
 
     it('finished the first item and starts on the second', () => {
-      let workItem1 = new WorkItem(1000);
-      let workItem2 = new WorkItem(500);
-      worker.work();
-      inbox.add(workItem1);
-      inbox.add(workItem2);
-
       jest.advanceTimersByTime(1000);
+
+      expect(inbox.items()).toEqual([]);
+      expect(inProgress.items()).toEqual([workItem2]);
+      expect(outbox.items()).toEqual([workItem1]);
+    });
+
+    it('almost finishes the second', () => {
+      jest.advanceTimersByTime(1499);
+
       expect(inbox.items()).toEqual([]);
       expect(inProgress.items()).toEqual([workItem2]);
       expect(outbox.items()).toEqual([workItem1]);
     });
 
     it('finished the second item', () => {
-      let workItem1 = new WorkItem(1000);
-      let workItem2 = new WorkItem(500);
-      worker.work();
-      inbox.add(workItem1);
-      inbox.add(workItem2);
-
       jest.advanceTimersByTime(1500);
+
       expect(inbox.items()).toEqual([]);
       expect(inProgress.items()).toEqual([]);
       expect(outbox.items()).toEqual([workItem1, workItem2]);
