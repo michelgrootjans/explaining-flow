b3e6d794c7a04e5d0cfbfcbb765eb40452a35b03
Maarten Groeneweg <maartengroeneweg@funda.nl>
2021-02-24 10:42:22 +0100

    moved bundle to script to support windows

---
+++ bundle.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+const browserify = require('browserify');
+const path = require('path');
+const open = require('open');
+const promisify = require('util').promisify;
+
+async function bundle() {
+  await fs.rmdir('dist', { recursive: true });
+  await fs.mkdir('dist');
+  const sourceFiles = await getSourceFiles();
+  const bundle = await createBundle(sourceFiles);
+  await fs.writeFile(path.join('dist', 'index.js'), bundle, 'utf8');
+  await open('index.html', { wait: true });
+}
+
+async function createBundle(files) {
+  const b = browserify();
+  files.forEach(f=> b.add(f));
+  var doBundle = promisify(b.bundle.bind(b));
+  var buf = await doBundle()
+  return buf;
+}
+
+async function getSourceFiles() {
+  const files = await fs.readdir('src');
+  const jsFiles = files.filter(f=> f.endsWith('.js'));
+  return jsFiles.map(f=> path.join('src', f));
+}
+
+bundle();
