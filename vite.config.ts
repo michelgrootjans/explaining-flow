6d6fd1572ba3294039227c10bba091f9ca706afd
Michel Grootjans <michel.grootjans@gmail.com>
2026-03-23 17:16:07 +0100

    Set Vite base path to `/explaining-flow/` for correct build deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/explaining-flow/' : '/',
   build: {
     outDir: 'dist',
   },
-});
+}));
